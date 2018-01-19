a3bd8c419dd35627bea27fd450831b52a5e71b01
igorivacic2740 <igor.ivacic.2740@gmail.com>
2018-01-19 13:13:47 +0100

    Added temp dir for file uploads

--- src/main/java/com/pictogram/pictogram/commons/storage/StorageProperties.java
+++ src/main/java/com/pictogram/pictogram/commons/storage/StorageProperties.java
@@ -10,7 +10,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
  */
 @ConfigurationProperties("storage")
 public class StorageProperties {
-  private String location = "C://";
+  private String location = "upload/";
 
   public String getLocation() {
     return location;
